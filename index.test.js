5083773addaac68b4cd35f300279de23f09321ff
Endalk Yaregal <endalkachewyaregal747@gmail.com>
2026-03-15 20:11:04 +0300

    fix linting and testing files conflict

--- index.test.js
+++ index.test.js
@@ -1,8 +1,11 @@
-const { describe, it, expect } = require('jest');
 const sum = require('./index.js');
 
+/* global describe, it, expect */
+
 describe('sum function checking', () => {
-    it('sum has to return the correct sum of two numbers', () => {
+    it('sun has to return the correct sum of two numbers', () => {
         expect(sum(3, 5)).toBe(8);
+        expect(sum(0, 0)).toBe(0);
+        expect(sum(2, 2)).toBe(4);
     });
 });
